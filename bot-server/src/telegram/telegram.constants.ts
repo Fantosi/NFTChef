83ff8b7e242456b6e2d97edb0f2c07789d93bb1d
youseop <edlsw@naver.com>
2023-03-04 16:03:55 +0900

    Add switch-case format to define command

--- bot-server/src/telegram/telegram.constants.ts
+++ bot-server/src/telegram/telegram.constants.ts
@@ -1,1 +1,4 @@
 export const TEST_USER_ID = process.env.TEST_CHAT_ID;
+
+export const START_COMMAND = 'start';
+export const ECHO_COMMAND = 'echo';
---
+++ bot-server/src/util/parseText.ts
@@ -0,0 +1,19 @@
+export const isCommand = (text: string, command: string) => {
+  if (text.includes(`/${command}`)) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+export const getCommand = (text: string) => {
+  const firstChunk = text.split(' ')[0];
+  if (firstChunk[0] === '/') {
+    return firstChunk.slice(1);
+  }
+  return undefined;
+};
+
+export const getContentFromCommand = (text: string) => {
+  return text.split(' ').slice(1).join(' ');
+};
