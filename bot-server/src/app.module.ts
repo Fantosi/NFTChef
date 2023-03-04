11f461a7b002e9e275a6bbfe69a5ff4648b582ae
youseop <edlsw@naver.com>
2023-03-04 15:48:42 +0900

    Implement telegram chat bot to the project

--- bot-server/src/app.module.ts
+++ bot-server/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PaymentModule } from './payment/payment.module';
 import { TonModule } from './ton/ton.module';
+import { TelegramModule } from './telegram/telegram.module';
 
 @Module({
-  imports: [PaymentModule, TonModule],
+  imports: [PaymentModule, TonModule, TelegramModule],
   controllers: [AppController],
   providers: [AppService],
 })
